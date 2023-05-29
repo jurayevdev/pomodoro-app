328917778ec0f071f0221f3328c7aa85dba967f2
jurayevcoder <davlatjurayev.1@gmail.com>
2023-05-29 21:42:04 +0500

    time ok

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@
       <div class="timer">
         <div class="status"><img src="./assets/cofe.svg" alt="" /> STATUS</div>
         <div class="numbers">
-          <h1>04</h1>
-          <h1>59</h1>
+          <h1 id="min">25</h1>
+          <h1 id="sekunt">00</h1>
         </div>
         <div class="controls flex gap-5 text-4xl">
           <button class="p-6 bg-[rgba(77,218,110,0.15)] rounded-[24px]">
             <i class="bx bx-dots-horizontal-rounded"></i>
           </button>
-          <button class="p-6 bg-[rgba(77,218,110,0.15)] rounded-[24px]">
-            <i class="bx bx-play"></i>
+          <button @click="btn" id="play" class="p-6 bg-[rgba(77,218,110,0.15)] rounded-[24px]">
+            <i :class="!lamp ? 'bx bx-play' : 'bx bx-pause'"></i>
           </button>
           <button class="p-6 bg-[rgba(77,218,110,0.15)] rounded-[24px]">
             <i class="bx bx-skip-next"></i>
@@ -23,7 +23,53 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+
+const lamp = ref(false);
+const minute = ref(25);
+const end = ref(60);
+const setTimeOutResult = ref(null);
+
+let btn = () => {
+  lamp.value = !lamp.value;
+
+  if (lamp.value) {
+
+    setTimeOutResult.value = setInterval(() => {
+      end.value = end.value - 1;
+      sekunt.textContent = end.value;
+
+      if (end.value < 10) {
+        sekunt.textContent = `0${end.value}`;
+      }
+      if (end.value == 0 && minute.value !== -1) {
+        end.value = 60;
+        minute.value = minute.value - 1;
+
+        if (minute.value < 10) {
+          min.textContent = `0${minute.value}`;
+        } else {
+          min.textContent = minute.value;
+        }
+      }
+
+      if (minute.value == -1) {
+        clearInterval(setTimeOutResult.value);
+        minute.value = 25;
+        min.textContent = minute.value
+        end.value = 60;
+      }
+    }, 1000);
+  }
+
+  if (!lamp.value) {
+    clearInterval(setTimeOutResult.value);
+  }
+};
+
+onMounted(() => {});
+</script>
 
 <style lang="scss" scoped>
 .timer {
