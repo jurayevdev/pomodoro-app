922b5cb2333d4e48fbfcfa85df18f6014081b773
jurayevcoder <davlatjurayev.1@gmail.com>
2023-06-01 06:08:09 +0500

    dark mod

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
-  <div :class="classBody">
+  <div :class="dark ? classDarkBody : classBody">
     <div class="container grid place-items-center h-screen mx-auto">
-      <button
-        @click="toggleModal"
-        :class="
-          work
-            ? 'absolute right-4 top-4 text-[30px] text-[#14401d]'
-            : 'absolute right-4 top-4 text-[30px] text-[#471515]'
-        "
-      >
+      <button @click="toggleModal" :class="dark ? classDarkSetting : classSetting">
         <i :class="modal ? 'bx bx-x' : 'bx bx-cog'"></i>
       </button>
       <div id="dropdown" :class="modal ? classModal : 'hidden'">
@@ -21,15 +14,30 @@
         </div>
         <div class="p-4 flex items-center justify-between">
           Focus length
-          <input @click="subFocus" v-model="inputNumber.focus" type="number" class="w-20 rounded-lg" />
+          <input
+            @click="subFocus"
+            v-model="inputNumber.focus"
+            type="number"
+            class="w-20 rounded-lg"
+          />
         </div>
         <div class="p-4 flex items-center justify-between">
           Short break length
-          <input @click="subSHbreak" v-model="inputNumber.shBreak" type="number" class="w-20 rounded-lg" />
+          <input
+            @click="subSHbreak"
+            v-model="inputNumber.shBreak"
+            type="number"
+            class="w-20 rounded-lg"
+          />
         </div>
         <div class="p-4 flex items-center justify-between">
           Long break length
-          <input @click="subLbreak" v-model="inputNumber.lBreak" type="number" class="w-20 rounded-lg" />
+          <input
+            @click="subLbreak"
+            v-model="inputNumber.lBreak"
+            type="number"
+            class="w-20 rounded-lg"
+          />
         </div>
         <div class="p-4 flex items-center justify-between">
           Auto resume timer
@@ -42,21 +50,22 @@
       </div>
 
       <div class="timer">
-        <div :class="classStatus">
-          <img :src="!work ? miyya : cofe" alt="" />{{ status }}
+        <div :class="dark ? classDarkStatus : classStatus">
+          <i :class="work ? 'bx bx-coffee text-[30px]' : 'bx bx-brain text-[30px]'"></i
+          >{{ status }}
         </div>
         <div class="numbers">
-          <h1 id="min" :class="classNumber">25</h1>
-          <h1 id="sekunt" :class="classNumber">00</h1>
+          <h1 id="min" :class="dark ? classDarkNumber : classNumber">25</h1>
+          <h1 id="sekunt" :class="dark ? classDarkNumber : classNumber">00</h1>
         </div>
         <div class="controls flex gap-5 text-4xl">
-          <button @click="refresh" :class="classBtn">
+          <button @click="refresh" :class="dark ? classDarkBtn : classBtn">
             <i class="bx bx-refresh"></i>
           </button>
-          <button @click="btn" id="play" :class="classBtn">
+          <button @click="btn" id="play" :class="dark ? classDarkBtn : classBtn">
             <i :class="!lamp ? 'bx bx-play' : 'bx bx-pause'"></i>
           </button>
-          <button @click="next" :class="classBtn">
+          <button @click="next" :class="dark ? classDarkBtn : classBtn">
             <i class="bx bx-skip-next"></i>
           </button>
         </div>
@@ -67,8 +76,6 @@
 
 <script setup>
 import { ref, onMounted, reactive } from "vue";
-import miyya from "./assets/miyya.svg";
-import cofe from "./assets/cofe.svg";
 
 const modal = ref(false);
 const toggleModal = () => (modal.value = !modal.value);
@@ -90,17 +97,32 @@ const end = ref(60);
 const setTimeOutResult = ref(null);
 const work = ref(false);
 const sch = ref(0);
+
+const classSetting = ref("absolute right-4 top-4 text-[30px] text-[#471515]");
+const classDarkSetting = ref("absolute right-4 top-4 text-[30px] text-[#FFF2F2]");
+
 const classModal = ref(
+  "absolute top-16 bg-[#FFF2F2] right-5 md:top-12 md:right-12 z-10 rounded-[24px] shadow-lg w-[350px] text-[#471515]"
+);
+const classDarkModal = ref(
   "absolute top-16 bg-[#FFF2F2] right-5 md:top-12 md:right-12 z-10 rounded-[24px] shadow-lg w-[350px] text-[#471515]"
 );
 const classBtn = ref(
   "p-6 bg-[rgba(255,76,76,0.15)] rounded-[24px] text-[#471515] hover:bg-[#ee8989] focus:bg-[rgba(255,76,76,0.71)]"
 );
+const classDarkBtn = ref(
+  "p-6 bg-[#471515] rounded-[24px] text-[#FFF2F2] hover:bg-[rgba(255,76,76,0.55)] focus:bg-[rgba(255,76,76,0.71)]"
+);
 const classBody = ref("wrapper w-full min-h-screen bg-[#FFF2F2]");
+const classDarkBody = ref("wrapper w-full min-h-screen bg-[#0D0404]");
 const classStatus = ref(
   "status border-2 border-[#471515] text-[#471515] bg-[rgba(255,76,76,0.15)]"
 );
+const classDarkStatus = ref(
+  "status border-2 border-[#FFF2F2] text-[#FFF2F2] bg-[#471515]"
+);
 const classNumber = ref("text-[#471515]");
+const classDarkNumber = ref("text-[#FFF2F2]");
 
 // ----------------- submit imput start --------------- //
 
@@ -114,12 +136,12 @@ let subFocus = () => {
     }
     if (autoResume.value == true) {
       clearInterval(setTimeOutResult.value);
-      lamp.value = false
+      lamp.value = false;
     }
     sekunt.textContent = "00";
     end.value = 60;
   }
-}
+};
 
 let subSHbreak = () => {
   if (status.value == "SHORT BREAK") {
@@ -131,12 +153,12 @@ let subSHbreak = () => {
     }
     if (autoResume.value == true) {
       clearInterval(setTimeOutResult.value);
-      lamp.value = false
+      lamp.value = false;
     }
     sekunt.textContent = "00";
     end.value = 60;
   }
-}
+};
 
 let subLbreak = () => {
   if (status.value == "LONG BREAK") {
@@ -148,7 +170,7 @@ let subLbreak = () => {
     }
     if (autoResume.value == true) {
       clearInterval(setTimeOutResult.value);
-      lamp.value = false
+      lamp.value = false;
     }
     sekunt.textContent = "00";
     end.value = 60;
@@ -218,12 +240,25 @@ let next = () => {
       sch.value = 0;
       classModal.value =
         "absolute top-16 bg-[#F2FFF5] right-5 md:top-12 md:right-12 z-10 rounded-[24px] shadow-lg w-[350px] text-[#153047]";
+
       classBtn.value =
         "p-6 bg-[rgba(76,172,255,0.15)] rounded-[24px] text-[#153047] hover:bg-[rgba(76,172,255,0.40)] focus:bg-[rgba(76,172,255,0.62)]";
+      classDarkBtn.value =
+        "p-6 bg-[#153047] rounded-[24px] text-[#F2F9FF] hover:bg-[rgba(76,172,255,0.40)] focus:bg-[rgba(76,172,255,0.62)]";
+
       classBody.value = "wrapper w-full min-h-screen bg-[#F2F9FF]";
+      classDarkBody.value = "wrapper w-full min-h-screen bg-[#04090D]";
+
       classStatus.value =
         "status border-2 border-[#153047] text-[#153047] bg-[rgba(76,172,255,0.15)]";
+      classDarkStatus.value =
+        "status border-2 border-[#F2F9FF] text-[#F2F9FF] bg-[#153047]";
+
       classNumber.value = "text-[#153047]";
+      classDarkNumber.value = "text-[#F2F9FF]";
+
+      classSetting.value = "absolute right-4 top-4 text-[30px] text-[#153047]";
+      classDarkSetting.value = "absolute right-4 top-4 text-[30px] text-[#F2F9FF]";
     } else {
       minute.value = inputNumber.shBreak - 1;
       if (inputNumber.shBreak < 10) {
@@ -238,12 +273,25 @@ let next = () => {
       sch.value = sch.value + 1;
       classModal.value =
         "absolute top-16 bg-[#F2FFF5] right-5 md:top-12 md:right-12 z-10 rounded-[24px] shadow-lg w-[350px] text-[#14401D]";
+
       classBtn.value =
         "p-6 bg-[rgba(77,218,110,0.15)] rounded-[24px] text-[#14401D] hover:bg-[#86e299] focus:bg-[rgba(77,218,110,0.62)]";
+      classDarkBtn.value =
+        "p-6 bg-[#14401D] rounded-[24px] text-[#F2FFF5] hover:bg-[rgba(77,218,110,0.40)] focus:bg-[rgba(77,218,110,0.62)]";
+
       classBody.value = "wrapper w-full min-h-screen bg-[#F2FFF5]";
+      classDarkBody.value = "wrapper w-full min-h-screen bg-[#040D06]";
+
       classStatus.value =
         "status border-2 border-[#14401d] text-[#14401d] bg-[rgba(77,218,110,0.15)]";
+      classDarkStatus.value =
+        "status border-2 border-[#F2FFF5] text-[#F2FFF5] bg-[#14401D]";
+
       classNumber.value = "text-[#14401d]";
+      classDarkNumber.value = "text-[#F2FFF5]";
+
+      classSetting.value = "absolute right-4 top-4 text-[30px] text-[#14401d]";
+      classDarkSetting.value = "absolute right-4 top-4 text-[30px] text-[#F2FFF5]";
     }
   } else {
     minute.value = inputNumber.focus - 1;
@@ -258,12 +306,25 @@ let next = () => {
     lamp.value = false;
     classModal.value =
       "absolute top-16 bg-[#FFF2F2] right-5 md:top-12 md:right-12 z-10 rounded-[24px] shadow-lg w-[350px] text-[#471515]";
+
     classBtn.value =
       "p-6 bg-[rgba(255,76,76,0.15)] rounded-[24px] text-[#471515] hover:bg-[#ee8989] focus:bg-[rgba(255,76,76,0.71)]";
+    classDarkBtn.value =
+      "p-6 bg-[#471515] rounded-[24px] text-[#FFF2F2] hover:bg-[rgba(255,76,76,0.55)] focus:bg-[rgba(255,76,76,0.71)]";
+
     classBody.value = "wrapper w-full min-h-screen bg-[#FFF2F2]";
+    classDarkBody.value = "wrapper w-full min-h-screen bg-[#0D0404]";
+
     classStatus.value =
       "status border-2 border-[#471515] text-[#471515] bg-[rgba(255,76,76,0.15)]";
+    classDarkStatus.value =
+      "status border-2 border-[#FFF2F2] text-[#FFF2F2] bg-[#471515]";
+
     classNumber.value = "text-[#471515]";
+    classDarkNumber.value = "text-[#FFF2F2]";
+
+    classSetting.value = "absolute right-4 top-4 text-[30px] text-[#471515]";
+    classDarkSetting.value = "absolute right-4 top-4 text-[30px] text-[#FFF2F2]";
   }
   work.value = !work.value;
 };
@@ -311,12 +372,25 @@ let btn = () => {
             status.value = "LONG BREAK";
             classModal.value =
               "absolute top-16 bg-[#F2FFF5] right-5 md:top-12 md:right-12 z-10 rounded-[24px] shadow-lg w-[350px] text-[#153047]";
+
             classBtn.value =
               "p-6 bg-[rgba(76,172,255,0.15)] rounded-[24px] text-[#153047] hover:bg-[rgba(76,172,255,0.40)] focus:bg-[rgba(76,172,255,0.62)]";
+            classDarkBtn.value =
+              "p-6 bg-[#153047] rounded-[24px] text-[#F2F9FF] hover:bg-[rgba(76,172,255,0.40)] focus:bg-[rgba(76,172,255,0.62)]";
+
             classBody.value = "wrapper w-full min-h-screen bg-[#F2F9FF]";
+            classDarkBody.value = "wrapper w-full min-h-screen bg-[#04090D]";
+
             classStatus.value =
               "status border-2 border-[#153047] text-[#153047] bg-[rgba(76,172,255,0.15)]";
+            classDarkStatus.value =
+              "status border-2 border-[#F2F9FF] text-[#F2F9FF] bg-[#153047]";
+
             classNumber.value = "text-[#153047]";
+            classDarkNumber.value = "text-[#F2F9FF]";
+
+            classSetting.value = "absolute right-4 top-4 text-[30px] text-[#153047]";
+            classDarkSetting.value = "absolute right-4 top-4 text-[30px] text-[#F2F9FF]";
           } else {
             minute.value = inputNumber.shBreak - 1;
             if (inputNumber.shBreak < 10) {
@@ -328,12 +402,25 @@ let btn = () => {
             status.value = "SHORT BREAK";
             classModal.value =
               "absolute top-16 bg-[#F2FFF5] right-5 md:top-12 md:right-12 z-10 rounded-[24px] shadow-lg w-[350px] text-[#14401D]";
+
             classBtn.value =
               "p-6 bg-[rgba(77,218,110,0.15)] rounded-[24px] text-[#14401D] hover:bg-[#86e299] focus:bg-[rgba(77,218,110,0.62)]";
+            classDarkBtn.value =
+              "p-6 bg-[#14401D] rounded-[24px] text-[#F2FFF5] hover:bg-[rgba(77,218,110,0.40)] focus:bg-[rgba(77,218,110,0.62)]";
+
             classBody.value = "wrapper w-full min-h-screen bg-[#F2FFF5]";
+            classDarkBody.value = "wrapper w-full min-h-screen bg-[#040D06]";
+
             classStatus.value =
               "status border-2 border-[#14401d] text-[#14401d] bg-[rgba(77,218,110,0.15)]";
+            classDarkStatus.value =
+              "status border-2 border-[#F2FFF5] text-[#F2FFF5] bg-[#14401D]";
+
             classNumber.value = "text-[#14401d]";
+            classDarkNumber.value = "text-[#F2FFF5]";
+
+            classSetting.value = "absolute right-4 top-4 text-[30px] text-[#14401d]";
+            classDarkSetting.value = "absolute right-4 top-4 text-[30px] text-[#F2FFF5]";
           }
         } else {
           minute.value = inputNumber.focus - 1;
@@ -345,12 +432,25 @@ let btn = () => {
           status.value = "FOCUS";
           classModal.value =
             "absolute top-16 bg-[#FFF2F2] right-5 md:top-12 md:right-12 z-10 rounded-[24px] shadow-lg w-[350px] text-[#471515]";
+
           classBtn.value =
             "p-6 bg-[rgba(255,76,76,0.15)] rounded-[24px] text-[#471515] hover:bg-[#ee8989] focus:bg-[rgba(255,76,76,0.71)]";
+          classDarkBtn.value =
+            "p-6 bg-[#471515] rounded-[24px] text-[#FFF2F2] hover:bg-[rgba(255,76,76,0.55)] focus:bg-[rgba(255,76,76,0.71)]";
+
           classBody.value = "wrapper w-full min-h-screen bg-[#FFF2F2]";
+          classDarkBody.value = "wrapper w-full min-h-screen bg-[#0D0404]";
+
           classStatus.value =
             "status border-2 border-[#471515] text-[#471515] bg-[rgba(255,76,76,0.15)]";
+          classDarkStatus.value =
+            "status border-2 border-[#FFF2F2] text-[#FFF2F2] bg-[#471515]";
+
           classNumber.value = "text-[#471515]";
+          classDarkNumber.value = "text-[#FFF2F2]";
+
+          classSetting.value = "absolute right-4 top-4 text-[30px] text-[#471515]";
+          classDarkSetting.value = "absolute right-4 top-4 text-[30px] text-[#FFF2F2]";
         }
         end.value = 60;
         if (autoResume.value == false) {
@@ -394,7 +494,7 @@ onMounted(() => {});
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 20px;
+  gap: 10px;
 }
 
 .controls {
